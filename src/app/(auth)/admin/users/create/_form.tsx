ccbeabf086d84d44fdaf6d0e9032d0e2379617c0
andresjesse <andres.porfirio.tablet@gmail.com>
2024-04-23 19:57:54 -0300

    removed extra fragment

--- src/app/(auth)/admin/users/create/_form.tsx
+++ src/app/(auth)/admin/users/create/_form.tsx
@@ -11,56 +11,54 @@ export function CreateUserForm() {
   const [state, formAction] = useFormState(createUserAction, undefined);
 
   return (
-    <>
-      <Form action={formAction}>
-        {state && (
-          <Form.FlashMessage
-            type={state.type}
-            message={state.message}
-          />
-        )}
-
-        <Form.Input
-          name="name"
-          label="Nome"
-          placeholder="Nome"
-          withAsterisk
-          errors={state?.errors?.["name"]}
+    <Form action={formAction}>
+      {state && (
+        <Form.FlashMessage
+          type={state.type}
+          message={state.message}
         />
+      )}
 
-        <Form.Input
-          name="email"
-          label={t("auth.email")}
-          placeholder="user@email.com"
-          withAsterisk
-          errors={state?.errors?.["email"]}
-        />
+      <Form.Input
+        name="name"
+        label="Nome"
+        placeholder="Nome"
+        withAsterisk
+        errors={state?.errors?.["name"]}
+      />
 
-        <Form.Input
-          name="password"
-          label={t("auth.password")}
-          type="password"
-          placeholder="********"
-          withAsterisk
-          errors={state?.errors?.["password"]}
-        />
+      <Form.Input
+        name="email"
+        label={t("auth.email")}
+        placeholder="user@email.com"
+        withAsterisk
+        errors={state?.errors?.["email"]}
+      />
 
-        <Form.Input
-          name="password_confirmation"
-          label={t("auth.password_confirmation")}
-          type="password"
-          placeholder="********"
-          withAsterisk
-          errors={state?.errors?.["password_confirmation"]}
-        />
+      <Form.Input
+        name="password"
+        label={t("auth.password")}
+        type="password"
+        placeholder="********"
+        withAsterisk
+        errors={state?.errors?.["password"]}
+      />
 
-        <Form.Button
-          className="self-end"
-          data-testid={testIDs.SUBMIT_BUTTON}
-        >
-          {t(`general_actions.create`)}
-        </Form.Button>
-      </Form>
-    </>
+      <Form.Input
+        name="password_confirmation"
+        label={t("auth.password_confirmation")}
+        type="password"
+        placeholder="********"
+        withAsterisk
+        errors={state?.errors?.["password_confirmation"]}
+      />
+
+      <Form.Button
+        className="self-end"
+        data-testid={testIDs.SUBMIT_BUTTON}
+      >
+        {t(`general_actions.create`)}
+      </Form.Button>
+    </Form>
   );
 }
