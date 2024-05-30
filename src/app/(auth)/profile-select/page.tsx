fcbf2e66b5f2e63fd604cb819e9ab077345a98b2
leivans <samboeira@gmail.com>
2024-05-30 20:10:18 -0300

    added profile select page

--- src/app/(auth)/profile-select/page.tsx
+++ src/app/(auth)/profile-select/page.tsx
@@ -1,14 +1,59 @@
+import auth from "@/auth";
 import { VBox } from "@/components/containers";
-import Link from "next/link";
+import SideBarLink from "@/components/layouts/DashboardLayout/SideBar/SideBarLink";
+import { Heading } from "@/components/ui";
+import { Roles } from "@/enums/Roles";
+import { t } from "@/lang";
+import Image from "next/image";
+import { FaUserSecret } from "react-icons/fa";
+import { FaGear, FaUserLarge } from "react-icons/fa6";
 
-export default function page() {
+export default async function page() {
+  const { user } = await auth();
+
   return (
-    <VBox>
-      <Link href="/admin">Link to Admin</Link>
+    <div className="m-10 flex flex-col-reverse items-center lg:flex-row">
+      <div className="lg:w-[50%]">
+        <Heading h1>{t("page_titles.profile_select", { name: user.name })}</Heading>
 
-      <Link href="/manager">Link to Manager</Link>
+        <VBox className="mt-4">
+          {user.roles_ids?.includes(1) && (
+            <SideBarLink
+              icon={<FaUserSecret />}
+              href={`/${Roles.ADMIN}`}
+            >
+              {t(`roles.${Roles.ADMIN}`)}
+            </SideBarLink>
+          )}
 
-      <Link href="/social-assistant">Link to Social assistant</Link>
-    </VBox>
+          {user.roles_ids?.includes(2) && (
+            <SideBarLink
+              icon={<FaGear />}
+              href={`/${Roles.MANAGER}`}
+            >
+              {t(`roles.${Roles.MANAGER}`)}
+            </SideBarLink>
+          )}
+
+          {user.roles_ids?.includes(3) && (
+            <SideBarLink
+              icon={<FaUserLarge />}
+              href={`/${Roles.SOCIAL_ASSISTANT}`}
+            >
+              {t(`roles.${Roles.SOCIAL_ASSISTANT}`)}
+            </SideBarLink>
+          )}
+        </VBox>
+      </div>
+
+      <Image
+        className="w-full sm:mb-16 sm:w-[50%]"
+        src="/images/landing-page-image.svg"
+        alt="landing-page"
+        width="4000"
+        height="2667"
+        priority
+      />
+    </div>
   );
 }
