d76dafa6150e429257876cfe55bbc9315a282633
Samuel Boeira <samboeira@gmail.com>
2024-05-30 18:30:41 -0300

    @leivans/29/feature/landing-page (#31)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import Link from "next/link";
+import { HBox } from "@/components/containers";
+import { FullscreenLayout } from "@/components/layouts";
+import { Button, Heading, Text } from "@/components/ui";
+import { t } from "@/lang";
+import Image from "next/image";
 
 export default function page() {
   return (
-    <main>
-      <Link href="/login">Login</Link>
-    </main>
+    <FullscreenLayout.Root className="w-full">
+      <HBox className="m-5 justify-end">
+        <Button href="/login">{t("landing_page.login")}</Button>
+      </HBox>
+
+      <div className="m-10 flex flex-col-reverse items-center lg:flex-row">
+        <div className="lg:w-[50%]">
+          <Heading h1>{t("landing_page.title")}</Heading>
+
+          <Text className="mt-4 font-medium">{t("landing_page.subtitle")}</Text>
+
+          <Text className="mt-2 text-sm">{t("landing_page.content")}</Text>
+        </div>
+
+        <Image
+          className="w-full sm:mb-16 sm:w-[50%]"
+          src="/images/landing-page-image.svg"
+          alt="landing-page"
+          width="4000"
+          height="2667"
+          priority
+        />
+      </div>
+    </FullscreenLayout.Root>
   );
 }
